3acbde72b4ac78bcceb1bc612b015439a82a8ddf
Yuxuan Ding <54516574+SybariteDDD@users.noreply.github.com>
2021-02-24 16:38:10 -0600

    Add register view page and a file to store every constant

---
+++ 439FinalProject/439FinalProject/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  439FinalProject
+//
+//  Created by Kevin Ding on 2/24/21.
+//
+
+struct Constants {
+    static let loginSegue = "LoginToTrailMap"
+    static let registerSegue = "RegisterToTrailMap"
+}
