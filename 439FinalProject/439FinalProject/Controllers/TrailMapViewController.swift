2e580ce8edbcc74ee2e39410f59f2301354a3705
Andrew Wu <andrew.wu@wustl.edu>
2021-03-01 12:06:22 -0600

    Overpass API call now uses user's current location.

--- 439FinalProject/439FinalProject/Controllers/TrailMapViewController.swift
+++ 439FinalProject/439FinalProject/Controllers/TrailMapViewController.swift
@@ -16,20 +16,10 @@ class TrailMapViewController: UIViewController, MGLMapViewDelegate {
         let urlStyle = URL(string: "mapbox://styles/mapbox/streets-v11")
         let mapView = MGLMapView(frame: view.bounds, styleURL: urlStyle)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), zoomLevel: 9, animated: false)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 37.33233, longitude: -122.03122), zoomLevel: 9, animated: false)
         mapView.showsUserLocation = true
         mapView.delegate = self
         view.addSubview(mapView)
-        
-        let url = URL(string: "http://overpass-api.de/api/interpreter?data=[bbox];way[sac_scale=hiking];out;&bbox=-122.62939453125,36.836767364219,-121.27670288086,37.855338589498")!
-
-        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-            guard let data = data else { return }
-            print("test")
-            print(String(data: data, encoding: .utf8)!)
-        }
-
-        task.resume()
     }
     
 //    func mapView(_ mapView: MGLMapView, didUpdateUserLocation userLocation: Any!){
@@ -38,5 +28,17 @@ class TrailMapViewController: UIViewController, MGLMapViewDelegate {
     
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         mapView.setCenter((mapView.userLocation?.coordinate)!, animated: false)
+        //print("latitude: \(mapView.latitude), longitude: \(mapView.longitude)")
+        let lat = mapView.latitude
+        let lon = mapView.longitude
+        let box = [lon - 0.2526855444, lat - 0.4210251975, lon + 0.2526855444, lat + 0.4210251975]
+        let url = URL(string: "http://overpass-api.de/api/interpreter?data=[bbox];way[sac_scale=hiking];out;&bbox=\(box[0]),\(box[1]),\(box[2]),\(box[3])")!
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            print("test")
+            print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
     }
 }
