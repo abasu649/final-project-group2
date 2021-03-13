667608952ad9c664c587f6ad6e619f47759264d1
Andrew Wu <andrew.wu@wustl.edu>
2021-03-13 15:30:53 -0600

    Added some details for create hike page.

---
+++ 439FinalProject/439FinalProject/Controllers/CreateHikeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateHikeViewController.swift
+//  439FinalProject
+//
+//  Created by Andrew Wu on 3/13/21.
+//
+
+import UIKit
+import Firebase
+
+class CreateHikeViewController : UIViewController {
+    
+    @IBOutlet weak var miscDetails: UITextField!
+    override func viewDidLoad() {
+        miscDetails.borderStyle = UITextField.BorderStyle.roundedRect
+    }
+}
